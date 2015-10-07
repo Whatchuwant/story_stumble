e4e2447cd81fdbfb330aac50762b00c05ff169f9
Dan Ugelow <Dan.Ugelow@gmail.com>
2015-10-07 17:45:01 -0400

    added frontend structure

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './app'
+#requests mandrill gem for email contact
+require 'mandrill'
 #links models.rb to be used
 require './models'
 
 set(:database, 'sqlite3:jul_dan.sqlite3')
+
+get '/login' do
+	erb :login
+end
+
+get '/profile'
+	erb :profile
+end
+
+get '/discover'
+	erb :discover
+end
