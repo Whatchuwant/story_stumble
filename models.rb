4e2e6a7afeedec7791bad5944d80a86a5fc77359
Dan Ugelow <Dan.Ugelow@gmail.com>
2015-10-11 16:13:00 -0400

    updated models

--- models.rb
+++ models.rb
@@ -1,21 +1,25 @@
 #defines relationship betweeen tables
 class User < ActiveRecord::Base
-	has_many :posts
 	has_one :profile
-	has_many :follows, foreign_key: :followee_id
 end
 
 class Post < ActiveRecord::Base
 # reference to table must be singular, even if table name is plural
-	belongs_to :user
+	belongs_to :profile
 end
 
 class Profile < ActiveRecord::Base
 	belongs_to :user
+	has_many :posts
+	has_many :follows, foreign_key: :followee_id, class_name: "::Follow"
+	has_many :followings, foreign_key: :follower_id, class_name: "::Follow"
+	has_many :followers, through: :follows, class_name: Profile
+	has_many :followees, through: :followings, class_name: Profile
 end
 
 #when we want to see 
 
 class Follow < ActiveRecord::Base
-	has_many :follows, foreign_key: :follower_id
+	 belongs_to :follower, foreign_key: :follower_id, class_name: Profile
+	 belongs_to :followee, foreign_key: :followee_id, class_name: Profile
 end
