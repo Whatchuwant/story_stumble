2c175568791e39d82d1fa01e0544c1b7f1d68508
Dan Ugelow <Dan.Ugelow@gmail.com>
2015-10-11 18:51:10 -0400

    fixed style, added nav to other pages, added info page, fixing login info to database

--- app.rb
+++ app.rb
@@ -10,6 +10,10 @@ set(:database, 'sqlite3:jul_dan.sqlite3')
 
 enable :sessions
 
+get '/' do
+	erb :info
+end
+
 get '/login' do
 	erb :login
 end
@@ -26,3 +30,44 @@ get '/settings' do
 	erb :settings
 end
 
+#http POST method and '/login' action route
+post '/login' do
+	@user = User.where(email: params[:email]).first
+
+	if @user && @user.password == params[:password]
+		session[:user_id] = @user.id
+		flash[:info] = "You're now signed in"
+		redirect "/"
+
+	else
+		flash[:alert] = "Your password or email is incorrect"
+		redirect "/login"
+	end
+end
+
+# # HTTP GET method and "/signup" action route
+# get "/signup" do
+#   # tell the person that they are signed up and logged in
+#   flash[:info] = "You are now signed up and logged in"
+
+#   # this will output whatever is within the signup.erb template
+#   erb :signup
+# end
+
+post '/signup' do
+	@user = User.create(email: params[:email], password: params[:password])
+	session[:user_id] = @user.id
+	redirect '/'
+end
+
+get '/logout' do
+	session[:user_id] = nil
+	flash[:info] = "You are now logged out"
+	redirect '/'
+end
+
+def current_user
+  if session[:user_id]
+    @current_user = User.find(session[:user_id])
+  end
+end
